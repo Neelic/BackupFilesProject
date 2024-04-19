f8f47a998fc0365f96e2063896a8453dded95528
Neelic <salnikov.danila@mail.ru>
2024-04-19 20:14:22 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using BackupFilesProject.App;
+
+namespace BackupFilesProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Exceprion: No arguments!");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
